eebf34c44d3fb77b22b28449ae26b280c712a563
LalokeraXD <lalokeraxxx@gmail.com>
2023-11-13 09:24:56 -0700

    primer commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+require('./utils/mongoConnection');
+
+const booksRouter = require('./routers/books.router');
+
+const app = express();
+const port = 3003;
+
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+    res.send("Bienvenido a la Librería API");
+});
+
+app.use(express.json({limit: '50mb'}));
+app.use('/books', booksRouter);
+
+
+app.listen(port, () => {
+    console.log("Servidor iniciado en http://localhost:" + port);
+});
+
+
